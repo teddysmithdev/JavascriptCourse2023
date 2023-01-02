e02f74e5754eda0223eafb25f305886246030876
teddysmithdev <teddysmithdev@gmail.com>
2023-01-02 07:17:12 -0500

    String Escapes + Template Literals

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-let favoriteStingray = "cow nose stingray";
-favoriteStingray.indexOf("stingray"); //9
-favoriteStingray.indexOf("stringray"); // -1
+"don\'t shoot the snapping turtle";
 
-favoriteStingray.slice(9, 17); //'stingray'
+'that\'s awesome';
 
+"a backslash looks like this \\";
 
-favoriteStingray.replace("cow nose", "manta"); // 'manta stingray'
+"they call it \"ecscape\" character"
+
+"this string has two \n line";
+
+let favoriteAnimal = "bullfrog";
+let favoritePlaceToEat = "my house"
+
+`There is a huge ${favoriteAnimal} behind ${favoritePlaceToEat}`;
