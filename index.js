e9768a6a588dc38f6d9c0d4a26170de8b3205d2d
teddysmithdev <teddysmithdev@gmail.com>
2023-01-10 14:55:39 -0500

    Comparison Operators

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-let favoriteLizards = ['bearded dragon', 'salamander', 'anole', 'iguana'];
-favoriteLizards.slice(0, 2)
-console.log(favoriteLizards);
-favoriteLizards.splice(0, 2);
-console.log(favoriteLizards);
+let greaterThan = 3 > 2;
+let lessThan = 2 < 1;
+
+let greaterThanOrEqualTo = 3 >= 2;
+let lessThanOrEqualTo = 1 <= 3;
+
+let strictEquality = 1 === 1;
+let strictNonEquality = 1 !== 2;
+
+
+let looseEquality = 7 == '7';
+let strictEqualityString = 7 === '7';
+console.log(0 == false);
+console.log(null == undefined);
