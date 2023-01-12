babde7c50d0973f52b1de70dac70a133ef39a1f2
teddysmithdev <teddysmithdev@gmail.com>
2023-01-12 05:52:02 -0500

    If,ElseIf,Else

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-let greaterThan = 3 > 2;
-let lessThan = 2 < 1;
-
-let greaterThanOrEqualTo = 3 >= 2;
-let lessThanOrEqualTo = 1 <= 3;
-
-let strictEquality = 1 === 1;
-let strictNonEquality = 1 !== 2;
+let spider = "black widow";
 
 
-let looseEquality = 7 == '7';
-let strictEqualityString = 7 === '7';
-console.log(0 == false);
-console.log(null == undefined);
+if(spider === "black widow") {
+    console.log("DO NOT TOUCH");
+} else if (spider === "brown recluse") {
+    console.log("DO NOT TOUCH");
+} else {
+    console.log("Catch and study");
+}
