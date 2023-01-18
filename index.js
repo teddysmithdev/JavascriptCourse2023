d44b5359a5e133f137851cbfddd39e3f8a9e9b7b
teddysmithdev <teddysmithdev@gmail.com>
2023-01-18 15:13:11 -0500

    For Loops

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-const fishBite = {
-    name: "Brown Chub",
-    class: "K. Bigibbus",
-    nickName: "Topsail Drummer",
-    conservationStatus: "Least Concern"
+//CRUD
+//CREATE
+let turtleString = "turtle";
+//READ
+console.log(turtleString[0]);
+console.log(turtleString[1]);
+console.log(turtleString[2]);
+
+//FOR LOOP
+
+for(let i = 0; i < 9; i++) {
+    console.log(turtleString[i])
 }
 
-//CRUD
-
-//CREATE
-fishBite.isCool = false;
+// //CREATE
+let planes = ['apache', 'f22', 'c130', 'sr71'];
 
 //READ
-console.log(fishBite.name);
-console.log(fishBite["nickName"])
-
-//UPDATE
-fishBite.name = "BROWN CHUB"
-
-//DELETE
-delete fishBite.conservationStatus;
-
-
-
-console.log(fishBite);
-
-//Nested objects + nested arrays within objects
-const fishBiteExample2 = {
-    name: "Brown Chub",
-    class: "K. Bigibbus",
-    nickName: ['Grey Chub', 'Topsail Drummer'],
-    conservationStatus: "Least Concern",
-    metaData: {
-        picture: "http://wikipedia.org/brownchub",
-        longitude: "34'234'",
-        latitude: "23'24235'"
-    }
+for (let i = 0; i < planes.length; i++) {
+    console.log(planes[i]);
 }
 
-console.log(fishBiteExample2.nickName[0])
-console.log(fishBiteExample2.metaData.latitude)
+
+const bullFrogs = [
+    {
+        name:"great banjo toad",
+        location: "australia",
+        isThreatened: false
+    },
+    {
+        name: "american bullfrog",
+        location: "american",
+        isThreatened: false
+    }
+]
+
+for(let i = 0; i < bullFrogs.length; i++) {
+    console.log(`${bullFrogs[i].name} is located in ${bullFrogs[i].location}`)
+}
+
