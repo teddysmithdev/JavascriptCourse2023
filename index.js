72091ce0a5074685064551a8ba1f79b231283ab8
teddysmithdev <teddysmithdev@gmail.com>
2023-01-27 12:42:04 -0500

    Nested Loops

--- index.js
+++ index.js
@@ -1,40 +1,19 @@
-//CRUD
-//CREATE
-let turtleString = "turtle";
-//READ
-console.log(turtleString[0]);
-console.log(turtleString[1]);
-console.log(turtleString[2]);
-
-//FOR LOOP
-
-for(let i = 0; i < 9; i++) {
-    console.log(turtleString[i])
-}
-
-// //CREATE
-let planes = ['apache', 'f22', 'c130', 'sr71'];
-
-//READ
-for (let i = 0; i < planes.length; i++) {
-    console.log(planes[i]);
-}
-
-
-const bullFrogs = [
-    {
-        name:"great banjo toad",
-        location: "australia",
-        isThreatened: false
-    },
-    {
-        name: "american bullfrog",
-        location: "american",
-        isThreatened: false
+for (let i = 1; i <= 10; i++) {
+    console.log('Outer loop:', i);
+    for(let j = 1; j <= 3;j++) {
+        console.log('    Inner Loop', j);
     }
+}
+
+const airplanes = [
+    ['777', 'dc-10', 'bombadier'],
+    ['learjet', 'g6', 'gulstream']
 ]
 
-for(let i = 0; i < bullFrogs.length; i++) {
-    console.log(`${bullFrogs[i].name} is located in ${bullFrogs[i].location}`)
+for(let i = 0; i < airplanes.length; i++) {
+    let row = airplanes[i];
+    console.log(row);
+    for(let j = 0; j < row.length; j++) {
+        console.log(row[j]);
+    }
 }
-
