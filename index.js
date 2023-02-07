e974977cfe8f436644012e32b1a3c40f396b70b6
teddysmithdev <teddysmithdev@gmail.com>
2023-02-07 10:42:45 -0500

    Higher Order Functions

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
-// This
-console.log("Global")
-console.log(this);
+//Callback function
 
-function logDelarationThis() {
-    console.log("Declaration:")
-    console.log(this);
+const myFunc1 = (callBackFunc) => {
+    let value = 1;
+    callBackFunc(value);
 }
 
-const logExpressionThis = function() {
-    console.log("Expression:");
-    console.log(this);
-}
-var ipAddr = '192.168.1.1';
-const util = {
-    ipAddr: '8.8.8.8',
-    logIpAddr() {
-        console.log(`IP Address: ${this.ipAddr}`)
-    },
-    checkBrowser: function() {
-        console.log(window.navigator.userAgent);
-    },
-    logThis() {
-        console.log(this);
-    }
+// myFunc(function(value) {
+//     console.log(value);
+// });
+
+myFunc1(value => {
+    console.log(value);
+})
+
+
+// Return function from function
+function returnFunction() {
+    return function() { return 1 };
 }
 
-logDelarationThis();
-logExpressionThis();
-util.checkBrowser();
-util.logThis();
-util.logIpAddr();
+// returnFunction()();
+const myFunc = returnFunction();
+myFunc(); // => 1
